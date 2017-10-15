39a9cfadd4c8d684cb35504d0a96c12059290cd9
Yusuf Hamid <yusufh97@live.com>
2017-10-15 00:35:51 -0700

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,27 @@
+// Side navigation
+function click_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidenav").style.width = "25%";
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function click_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
+
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "navbar" + " w3-card-2" + " w3-animate-top" + " color-red";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top color-red", "");
+    }
+}
+
+
+
